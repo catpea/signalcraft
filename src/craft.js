8c7b07310efef4fa0771b99cd951af5f4bb9c87d
catpea <corporate.miou@gmail.com>
2023-12-18 22:16:16 -0500

    Make Compatible With Github Pages

--- src/craft.js
+++ src/craft.js
@@ -1,5 +1,8 @@
 // Boot Script - this is a boot sctipt that gets all the non-symmetrical oddities out of the way
 
+import bootstrapCss from 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrapJs from 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import Application from './application/Application.js';
 import setup from './setup.js';
 import usage from './usage.js';
